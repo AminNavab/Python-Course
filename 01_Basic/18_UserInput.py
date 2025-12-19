23e0f6c2eebdbb06ebed7bc7ed678a4948466c8a
AminNavabzade <aminnavabzade@gmail.com>
2025-12-19 18:54:54 +0330

    create input file

---
+++ 01_Basic/18_UserInput.py
@@ -0,0 +1,15 @@
+
+print("enter your name: ")
+name = input()
+print(f"your name is {name}")
+# OR:
+name2 = input("enter your name: ")
+print(f"your name is {name2}")
+#-----------------------------------------
+# NOTE: The input from the user is treated as a string.
+number = int(input("enter a integer number: "))
+number1 = float(input("enter a float number: "))
+name3 = str(input("enter a name: "))
+print(number)
+print(number1)
+print(name3)
