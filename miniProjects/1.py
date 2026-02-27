5a0a6ec6d9607c0ef3329b80163f99899b9ce694
AminNavabzade <aminnavabzade@gmail.com>
2026-02-27 03:44:05 +0330

    01 update

--- miniProjects/1.py
+++ miniProjects/1.py
@@ -12,3 +12,20 @@ def z (a,b):
 def t (a,b):
     result = a/b
     print(f"{a}/{b}={result}")
+
+def calculator():
+    a = int(input("number1 \n"))
+    b = int(input("number2 \n"))
+    operator = input("operator \n")
+    if operator=="+":
+        g (a,b)
+    elif operator=="-":
+        m (a,b)
+    elif operator=="*":
+        z (a,b)
+    elif operator=="/":
+        t (a,b)
+    else:
+        print("erorr")            
+
+print(calculator())  
