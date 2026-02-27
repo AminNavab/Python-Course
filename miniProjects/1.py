a1f4b42cfaeee2c79db4aa556ee0ed65a4a1fa8f
AminNavabzade <aminnavabzade@gmail.com>
2026-02-27 03:39:46 +0330

    01

---
+++ miniProjects/1.py
@@ -0,0 +1,14 @@
+# calculator(mashin hesab) 
+def g (a,b):
+    result = a+b
+    print(f"{a}+{b}={result}")
+def m (a,b):
+    result = a-b
+    print(f"{a}-{b}={result}")
+   
+def z (a,b):
+    result = a*b
+    print(f"{a}*{b}={result}")
+def t (a,b):
+    result = a/b
+    print(f"{a}/{b}={result}")
